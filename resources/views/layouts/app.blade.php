b219ae7715d9549c9e19ee9dc5794525dfcffa54
SD-Mdouka <sd.mdouka@gmail.com>
2022-04-21 22:10:59 +0200

    change project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,6 +65,7 @@
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
+                                        @method("GET")
                                     </form>
                                 </div>
                             </li>
